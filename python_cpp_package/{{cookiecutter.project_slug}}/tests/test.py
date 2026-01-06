e6e1c44c91158e84f4e506b78053e98d9abe5b1c
Johan Larsson <johan@jolars.co>
2026-01-06 15:50:21 +0100

    fix: fix tests

--- python_cpp_package/{{cookiecutter.project_slug}}/tests/test.py
+++ python_cpp_package/{{cookiecutter.project_slug}}/tests/test.py
@@ -1,14 +1,13 @@
 """Basic test for the {{ cookiecutter.project_name }} package."""
 
-from {{ cookiecutter.project_slug }} import MyClass, add
+from {{ cookiecutter.project_slug }} import MyClass, my_function
 
 def test_my_class():
     """Test the MyClass functionality."""
     obj = MyClass(10)
     assert obj.get_value() == 10
 
-def test_add_function():
+def test_my_function():
     """Test the add function."""
-    from {{ cookiecutter.project_slug }} import add
-    assert add(3, 5) == 8
-    assert add(-1, 1) == 0
+    assert my_function(3, 5) == 8
+    assert my_function(-1, 1) == 0
