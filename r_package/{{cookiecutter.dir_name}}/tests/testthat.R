5713b0f59dab5f1e58ff166294bf8e69245c7421
Johan Larsson <johan@jolars.co>
2026-01-30 15:48:43 +0100

    feat: add a r_package cookiecutter

---
+++ r_package/{{cookiecutter.dir_name}}/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library({{cookiecutter.package_name}})
+
+test_check("{{cookiecutter.package_name}}")
---
+++ r_package/{{cookiecutter.dir_name}}/tests/testthat/test-placeholder.R
@@ -0,0 +1,3 @@
+test_that("One plus one is two", {
+  expect_equal(1 + 1, 2)
+})
